8c1a24636b6cbf210a35ed6594629dbc42497236
Andrei B <andrei4.bulzan@gmail.com>
2018-05-11 19:56:48 +0300

    testing server

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 "use strict";
 // Optional. You will see this name in eg. 'ps' or 'top' command
 process.title = 'node-chat';
+
 // Port where we'll run the websocket server
 var webSocketsServerPort = 1337;
+
 // websocket and http servers
 var webSocketServer = require('websocket').server;
 var http = require('http');
+
 /**
  * Global variables
  */
-// latest 100 messages
+// latest 10 messages
 var history = [ ];
 // list of currently connected clients (users)
 var clients = [ ];
@@ -29,10 +32,12 @@ var server = http.createServer(function(request, response) {
   // Not important for us. We're writing WebSocket server,
   // not HTTP server
 });
+
 server.listen(process.env.PORT || webSocketsServerPort, function() {
   console.log((new Date()) + " Server is listening on port "
       + webSocketsServerPort);
 });
+
 /**
  * WebSocket server
  */
@@ -42,6 +47,7 @@ var wsServer = new webSocketServer({
   // http://tools.ietf.org/html/rfc6455#page-6
   httpServer: server
 });
+
 // This callback function is called every time someone
 // tries to connect to the WebSocket server
 wsServer.on('request', function(request) {
@@ -51,6 +57,7 @@ wsServer.on('request', function(request) {
   // make sure that client is connecting from your website
   // (http://en.wikipedia.org/wiki/Same_origin_policy)
   var connection = request.accept(null, request.origin); 
+
   // we need to know client index to remove them on 'close' event
   var index = clients.push(connection) - 1;
   var userName = false;
@@ -62,6 +69,7 @@ wsServer.on('request', function(request) {
     connection.sendUTF(
         JSON.stringify({ type: 'history', data: data_string } ));
   }
+
   // user sent some message
   connection.on('message', function(message) {
     if (message.type === 'utf8') { // accept only text
@@ -71,7 +79,8 @@ wsServer.on('request', function(request) {
         userName = htmlEntities(message.utf8Data);
         
         for (var i=0; i < clients.length; i++) {
-          clients[i].sendUTF(JSON.stringify({type: "notification_message", data: userName + " joined the group."}));
+        	if(clients[i] != connection)
+          		clients[i].sendUTF(JSON.stringify({type: "notification_message", data: userName + " joined the group."}));
         }
         
         console.log((new Date()) + ' User is known as: ' + userName);
@@ -94,7 +103,8 @@ wsServer.on('request', function(request) {
         var json = JSON.stringify({ type:'message', data: data_string });
 
         for (var i=0; i < clients.length; i++) {
-          clients[i].sendUTF(json);
+        	if(clients[i] != connection)
+          		clients[i].sendUTF(json);
         }
       }
     }
